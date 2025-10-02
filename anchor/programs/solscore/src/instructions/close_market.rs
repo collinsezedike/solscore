fc5c7f59726430e71f7dc079f1d070113782d521
Arokoyu Olaniyi <arokoyueb11@gmail.com>
2025-10-02 06:43:46 +0100

    added informational comment for close account

--- anchor/programs/solscore/src/instructions/close_market.rs
+++ anchor/programs/solscore/src/instructions/close_market.rs
@@ -7,6 +7,7 @@ use crate::state::{Bet, Market};
 pub fn _close_market(ctx: Context<CloseMarket>) -> Result<()> {
     let market = &mut ctx.accounts.market;
     require!(!market.is_closed, SolscoreError::MarketClosed);
+    // Market should be resolved before closing
     require!(market.is_resolved, SolscoreError::MarketNotResolved);
     market.is_closed=true;
     msg!("Market closed for betting: {} {}", market.league_name, market.season);
@@ -20,6 +21,7 @@ pub struct CloseMarket<'info> {
         has_one = admin,
         seeds = [b"market", market.league_name.as_bytes(), market.season.as_bytes()],
         bump = market.bump,
+        // Simple method for closing account and returnning rent back to admin
         close=admin
     )]
     pub market: Account<'info, Market>,
